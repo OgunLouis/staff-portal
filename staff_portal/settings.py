35f6768a186dc6c8d5c502921514ac4cf35458c1
OgunLouis <ogunnowolouis@gmail.com>
2025-07-23 16:47:16 +0100

    humanized

--- staff_portal/settings.py
+++ staff_portal/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'crispy_forms',
+    'django.contrib.humanize',
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
