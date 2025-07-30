cacfe7b6c7cfe5697a1687edbdeddebb6ccaf5d6
OgunLouis <ogunnowolouis@gmail.com>
2025-07-30 10:03:29 +0100

    debug off

--- staff_portal/settings.py
+++ staff_portal/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # Application definition
